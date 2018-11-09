dbd988654aa41209d0cc29d44706ba637eb99e1e
zdx1989 <18565653302@163.com>
2018-11-09 21:26:36 +0800

    FileSystem FsDataOutputStream sync

---
+++ src/main/scala/chap3/FileSync.scala
@@ -0,0 +1,29 @@
+package chap3
+
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+
+/**
+  * Created by zhoudunxiong on 2018/11/9.
+  */
+object FileSync {
+
+  def main(args: Array[String]): Unit = {
+    val uri = args(0)
+    val conf = new Configuration()
+    val fs = FileSystem.get(conf)
+    val p = new Path(uri)
+    val out = fs.create(p)
+    assert(fs.exists(p) == true)
+
+    out.write("content\n".getBytes("UTF-8"))
+    out.flush()
+    assert(fs.getFileStatus(p).getLen == 0L)
+
+    out.hsync()
+    //out.close()
+    assert(fs.getFileStatus(p).getLen > 0L)
+
+  }
+}

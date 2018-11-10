dc4e484353f9f1eeb785ee5544bfeb251061bb38
zdx1989 <18565653302@163.com>
2018-11-10 10:46:57 +0800

    hadoop  CodecPool get Compressor

---
+++ src/main/scala/chap4/PooledStreamCompressor.scala
@@ -0,0 +1,29 @@
+package chap4
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.io.IOUtils
+import org.apache.hadoop.io.compress.{CodecPool, CompressionCodec, Compressor}
+import org.apache.hadoop.util.ReflectionUtils
+
+/**
+  * Created by zhoudunxiong on 2018/11/10.
+  */
+object PooledStreamCompressor {
+
+  def main(args: Array[String]): Unit = {
+    val codecClassName = args(0)
+    val codecClass = Class.forName(codecClassName)
+    val conf = new Configuration()
+    val codec = ReflectionUtils.newInstance(codecClass, conf).asInstanceOf[CompressionCodec]
+    var compressor: Compressor = null
+    try {
+      compressor = CodecPool.getCompressor(codec)
+      val out = codec.createOutputStream(System.out)
+      IOUtils.copyBytes(System.in, out, 4096, false)
+      out.flush()
+    } finally {
+      CodecPool.returnCompressor(compressor)
+    }
+
+  }
+}

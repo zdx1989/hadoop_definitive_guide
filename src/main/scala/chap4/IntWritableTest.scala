df76e7b57a02016a417f157a8312f54886699c41
zdx1989 <18565653302@163.com>
2018-11-10 14:57:36 +0800

    hadoop  IntWritable serialize and deserialize test

--- src/main/scala/chap4/IntWritableTest.scala
+++ src/main/scala/chap4/IntWritableTest.scala
@@ -2,7 +2,7 @@ package chap4
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, DataInputStream, DataOutputStream}
 
-import org.apache.hadoop.io.{IntWritable, Writable}
+import org.apache.hadoop.io.{IntWritable, VIntWritable, Writable, WritableComparator}
 import org.apache.hadoop.util.StringUtils
 
 /**
@@ -20,6 +20,14 @@ object IntWritableTest {
     val newWritable = new IntWritable()
     deserialize(newWritable, bytes)
     assert(writable.get() == 163)
+
+    val comparator = WritableComparator.get(classOf[IntWritable])
+    val w1 = new IntWritable(163)
+    val w2 = new IntWritable(7)
+    assert(comparator.compare(w1, w2) > 0)
+
+    val data = serialize(new VIntWritable(163))
+    assert(StringUtils.byteToHexString(data) == "8fa3")
   }
 
   def serialize(writable: Writable): Array[Byte] = {

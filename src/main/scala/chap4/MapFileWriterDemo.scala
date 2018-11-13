2ac4c7440e3f4bb5dde520aab01d512ac185f360
zdx1989 <18565653302@163.com>
2018-11-13 20:38:46 +0800

    hadoop  io MapFile Writer

---
+++ src/main/scala/chap4/MapFileWriterDemo.scala
@@ -0,0 +1,36 @@
+package chap4
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.io.{IntWritable, MapFile, Text}
+
+/**
+  * Created by zhoudunxiong on 2018/11/13.
+  */
+object MapFileWriterDemo {
+
+  val DATA: Array[String] = Array(
+    "zdx",
+    "ygy",
+    "zdx and ygy"
+  )
+
+  def main(args: Array[String]): Unit = {
+    val uri = args(0)
+    val conf = new Configuration()
+    val fs = FileSystem.get(conf)
+    val key = new IntWritable()
+    val value = new Text()
+    var writer: MapFile.Writer = null
+    try {
+      writer = new MapFile.Writer(conf, fs, uri, key.getClass, value.getClass)
+      (0 until 1024).foreach { i =>
+        key.set(i + 1)
+        value.set(DATA(i % DATA.length))
+        writer.append(key, value)
+      }
+    } finally {
+      writer.close()
+    }
+  }
+}

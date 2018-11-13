a236e0b0045f4ec17326bc77fc2de410d67bcde7
zdx1989 <18565653302@163.com>
2018-11-13 23:23:13 +0800

    hadoop  mapreduce mapper

---
+++ src/main/scala/chap2/MaxTemperatureMapper.scala
@@ -0,0 +1,24 @@
+package chap2
+
+import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
+import org.apache.hadoop.mapreduce.Mapper
+
+/**
+  * Created by zhoudunxiong on 2018/11/13.
+  */
+class MaxTemperatureMapper extends Mapper[LongWritable, Text, Text, IntWritable] {
+
+  private val MISSING: Int = 9999
+
+  def map(key: LongWritable, value: Text, context: Context): Unit = {
+    val line = value.toString
+    val year = line.substring(15, 19)
+    val airTemperature =
+      if (line.charAt(87) == '+') line.substring(88, 92).toInt
+      else line.substring(87, 92).toInt
+    val quality = line.substring(92, 93)
+    if (airTemperature != MISSING && quality.matches("[01459]")) {
+      context.write(new Text(year), new IntWritable(airTemperature))
+    }
+  }
+}

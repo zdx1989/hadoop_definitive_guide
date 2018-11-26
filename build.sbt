85936d378a4535e5375ed56744ee3942f453813c
zdx1989 <18565653302@163.com>
2018-11-26 21:30:19 +0800

    hadoop  Zookeeper createGroup

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ libraryDependencies ++= Seq(
   "org.apache.hbase" % "hbase-client" % "2.0.0",
   "commons-logging" % "commons-logging" % "1.2",
   "org.apache.httpcomponents" % "httpclient" % "4.5.2",
+  "org.apache.zookeeper" % "zookeeper" % "3.4.10",
   "junit" % "junit" % "4.12" % "test",
   "org.hamcrest" % "hamcrest-all" % "1.3" % "test",
   "org.apache.mrunit" % "mrunit" % "1.1.0" % "test" classifier "hadoop2"
---
+++ src/main/scala/chap14/CreateGroup.scala
@@ -0,0 +1,51 @@
+package chap14
+
+import java.util.concurrent.CountDownLatch
+
+import org.apache.zookeeper.Watcher.Event.KeeperState
+import org.apache.zookeeper.ZooDefs.Ids
+import org.apache.zookeeper.{CreateMode, WatchedEvent, Watcher, ZooKeeper}
+
+/**
+  * Created by zhoudunxiong on 2018/11/26.
+  */
+class CreateGroup extends Watcher {
+
+  private val SEESSION_TIMEOUT = 5000
+
+  private val connectSignal = new CountDownLatch(1)
+
+  private var zk: ZooKeeper = _
+
+
+  def connect(hosts: String): Unit = {
+    zk = new ZooKeeper(hosts, SEESSION_TIMEOUT, this)
+    connectSignal.await()
+  }
+
+  override def process(event: WatchedEvent): Unit = {
+    if (event.getState == KeeperState.SyncConnected)
+      connectSignal.countDown()
+  }
+
+  def create(groupName: String): Unit = {
+    val path = "/" + groupName
+    val createPath = zk.create(path, null, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+    println(s"Created $createPath")
+  }
+
+  def close(): Unit = {
+    zk.close()
+  }
+
+}
+
+object CreateGroup {
+
+  def main(args: Array[String]): Unit = {
+    val createGroup = new CreateGroup()
+    createGroup.connect(args(0))
+    createGroup.create(args(1))
+    createGroup.close()
+  }
+}

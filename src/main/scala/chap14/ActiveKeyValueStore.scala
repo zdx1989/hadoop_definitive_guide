335649a046d36f3bfd4722eb1a72c96f652d774b
zdx1989 <18565653302@163.com>
2018-12-04 21:51:29 +0800

    hadoop  Zookeeper keyValueStore

--- src/main/scala/chap14/ActiveKeyValueStore.scala
+++ src/main/scala/chap14/ActiveKeyValueStore.scala
@@ -1,8 +1,9 @@
 package chap14
 
 import java.nio.charset.Charset
+import java.util.concurrent.TimeUnit
 
-import org.apache.zookeeper.{CreateMode, Watcher}
+import org.apache.zookeeper.{CreateMode, KeeperException, Watcher}
 import org.apache.zookeeper.ZooDefs.Ids
 
 /**
@@ -12,12 +13,26 @@ class ActiveKeyValueStore extends ConnectionWatcher {
 
   val CHARSET: Charset = Charset.forName("UTF-8")
 
+  val MAX_TRIES = 5
+
+  val RETRY_PERIOD_SECOND = 2
+
   def write(path: String, value: String): Unit = {
-    val stat = zk.exists(path, false)
-    if (stat == null) zk.create(path, value.getBytes(CHARSET),
-      Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
-    else
-      zk.setData(path, value.getBytes(CHARSET), -1)
+    var retires = 0
+    try {
+      val stat = zk.exists(path, false)
+      if (stat == null)
+        zk.create(path, value.getBytes(CHARSET), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+      else
+        zk.setData(path, value.getBytes(CHARSET), -1)
+    } catch {
+      case e: KeeperException.SessionExpiredException => throw e
+      case e: KeeperException =>
+        retires += 1
+        if (retires == MAX_TRIES) throw e
+        TimeUnit.SECONDS.sleep(RETRY_PERIOD_SECOND)
+
+    }
   }
 
   def read(path: String, watcher: Watcher): String = {

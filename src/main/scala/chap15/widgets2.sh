10f3cfed8852dcdc6ce377996d00fa86d579325b
zdx1989 <18565653302@163.com>
2018-12-09 20:21:49 +0800

    hadoop  sqoop sequenceFile

--- src/main/scala/chap15/widgets2.sh
+++ src/main/scala/chap15/widgets2.sh
@@ -7,3 +7,21 @@
  --as-sequencefile \
  --target-dir widget_sequence_files \
  --bindir .
+
+create table if not exists widgets2(
+    id int,
+    widget_name varchar(100),
+    price double,
+    design date,
+    version int,
+    notes varchar(200)
+);
+
+sqoop export \
+--connect jdbc:mysql://localhost/hadoopguide \
+--username root \
+--password zdx1989 \
+--table widgets2 \
+--class-name WidgetHolder \
+--jar-file WidgetHolder.jar \
+--export-dir widget_sequence_files
